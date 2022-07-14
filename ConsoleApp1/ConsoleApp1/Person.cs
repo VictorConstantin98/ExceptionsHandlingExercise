7d468ec59c63796e17472621d398d4eaf21a355e
Alexandru Crijman <alexandru.crijman@uipath.com>
2022-07-14 19:33:10 +0300

    Primele exceptii tratate

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public string getName() { 
+            return name;
+        }
+
+        public void setAge(int age)
+        {
+            if (age < 0) {
+                throw new ArgumentOutOfRangeException(age.ToString());
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,66 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            ///  Exception Example
+            ///  
+
+            // Structura try - care ruleaza o secventa de cod si o incearca. (Structura care este verificata)
+            try
+            {
+
+            }
+            catch (NullReferenceException exception)
+            {
+                /// Secventa de cod care se ruleaza in cazul in care se prinde NullReferenceException
+            }
+            // Prinde exceptia de tip Exception daca aceasta este aruncata in blocul de Try. Daca este aruncata atunci ruleaza urmatoarea secventa
+            catch (Exception exception)
+            {
+                // Secventa de cod care se ruleaza in cazul in care se prinde o exceptie de tip Exception
+
+
+            }
+
+
+            Person person  = null;
+
+            try
+            {
+                string name = person.getName();
+
+            }
+            catch (NullReferenceException exception) {
+                Console.WriteLine(exception.Message);
+            }
+
+            int a = 0;
+            int b = 5;
+
+            try
+            {
+                b = b / a;
+
+            }
+            catch (DivideByZeroException exception) {
+
+                Console.WriteLine("Incearca dinou cu alte numere");
+            }
+
+            Person person2 = new Person();
+
+            try
+            {
+                person2.setAge(-10);
+            }
+            // daca nu sti pui clasa parinte Exception
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
         }
     }
 }
